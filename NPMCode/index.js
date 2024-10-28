777505e40b3225a14d4021fc16f5acc2760860d5
sathwikraju <bunnyniceperson@gmail.com>
2024-10-28 12:41:36 +0530

    updated code

---
+++ NPMCode/index.js
@@ -0,0 +1,14 @@
+// var generateName = require("sillyname");
+
+import generateName from "sillyname";
+
+var sillyname = generateName();
+
+console.log(`My name is ${sillyname}.`);
+
+
+import { randomSuperhero } from "superheroes";
+
+const superHeroName = randomSuperhero();
+
+console.log(`I am ${superHeroName}!`);
